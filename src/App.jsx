4680fe691fb4be36c9322ab66941ab3f8cff42f4
bingak36 <102848043+bingak36@users.noreply.github.com>
2026-02-26 14:12:48 +0900

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,34 @@
-import { useState, useEffect } from 'react'
-import './style/main.scss'
-import Header from './components/Header'
-import Hero from './sections/Hero'
-import Footer from './components/Footer'
-import Follow from './sections/Follow'
-import Collection from './sections/Collection'
-import SkinCare from './sections/SkinCare'
-import Instargram from './sections/Instargram'
-import TopBanner from './components/TopBanner'
-import FixedTopBtn from './components/FixedTopBtn'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
-
-  const [topBanner, setTopBanner] = useState('')
-  const [isScrolled, setIsScrolled] = useState(false)
-
-  useEffect(()=>{
-
-    const handleScroll =()=>{
-      const scrollTop=window.scrollY
-
-      setIsScrolled(scrollTop>200)
-
-      // console.log(scrollTop)
-    }
-
-    window.addEventListener('scroll',handleScroll)
-
-  },[])
-
-  const upTopBanner =()=>{
-    setTopBanner('up')
-  }
   return (
-    <div className={`app-container ${topBanner} ${isScrolled? 'scrolled':''} `}>
-      <FixedTopBtn isScrolled={isScrolled} />
-      <TopBanner  onClick={upTopBanner}/>
-      <Header />
-      <main>
-        <section id="hero" className='section'>
-          <Hero />
-        </section>
-        <section id="follow" className='section'>
-          <Follow />
-        </section>
-        <section id="collection" className='section'>
-          <Collection />
-        </section>
-        <section id="skincare" className='section'>
-          <SkinCare />
-        </section>
-        <section id="instargram" className='section'>
-          <Instargram />
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
---
+++ src/section/TopScroll.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopScroll = () => {
+  return (
+    <div>TopScroll</div>
+  )
+}
+
+export default TopScroll
