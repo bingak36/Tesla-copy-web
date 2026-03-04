f2431120a3d20e32ae2e25f6c2b087db3e0078a1
bingak36 <102848043+bingak36@users.noreply.github.com>
2026-03-04 14:08:21 +0900

    12

--- src/Util/Header.js
+++ src/Util/Header.js
@@ -2,6 +2,12 @@ const img = (path) => `/tesla-image/${path}`
 
 export const NavData = {
 
+    logo: {
+        src: img('Logo.svg'),
+        alt: 'TESLA',
+        href: '/'
+    },
+
     menu: [
         { id: 'vehicles', label: '차량'},
         { id: 'charging', label: '충전'},
@@ -11,5 +17,8 @@ export const NavData = {
     
     icon: [
         {id: 'earth', icon:img("header-icon-1.svg")},
+        {id: 'search', icon:img("header-icon-2.svg")},
+        {id: 'acount', icon:img("header-icon-3.svg")},
+        {id: 'cart', icon:img("header-icon-4.svg")},
     ]
 }
--- src/section/Header.jsx
+++ src/section/Header.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import { NavData } from '../Util/Header'
+import './Styles/Header.scss'
 
 const Header = () => {
 
   const navLinks = NavData.menu
+  const logo = NavData.logo
+  const icon = NavData.icon
+  
+
   return (
     <div className='stiky-header'>  
-      <ul className='header-nav'>
+      <ul className='nav-text'>
         {navLinks.map((nav)=>(
           <li key={nav.id}>
             <a href={nav.href}>{nav.label}</a>
           </li>
         ))}
       </ul>
-
       
-      <div className='header-logo' />
-      <div>
-        
-      </div>
+      <a href={logo.href} className='logo'>
+        <img key={logo.id} src={logo.src} alt={logo.alt} />
+      </a>
+
+      <ul className='nav-icon'>
+        {icon.map((nav)=>(
+          <li key={nav.id}>
+            <a href={nav.href}>
+              <img src={nav.icon} alt={nav.alt} />
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
