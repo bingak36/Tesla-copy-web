be24ad219232f4c38c92be24efd2a6cedb777384
bingak36 <102848043+bingak36@users.noreply.github.com>
2026-03-12 19:47:48 +0900

    12

---
+++ src/Util/Footer.js
@@ -0,0 +1,13 @@
+const img = (path) => `/tesla-image/${path}`
+
+export const companyLogo = {
+    src: img("Logo.svg"),
+    alt: "TESLA-LOGO"
+}
+
+export const companyData = [
+
+    "테슬라 코리아 유한회사, 데이비드존파인스타인, 케네스어니스트무어",
+    "주소 : 서울특별시 강남구 테헤란로 419,9층",
+    
+]
