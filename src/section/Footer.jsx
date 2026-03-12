e81859c43732db411c2c1814f6b234fe8f6e0990
bingak36 <102848043+bingak36@users.noreply.github.com>
2026-03-12 19:43:53 +0900

    12

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <section className='company'>
+      <div className='company-logo'></div>
+      <div className='company-data'></div>
+      <div className='footerLegal'></div>
+      </section>
+
+      <section className='menu'>
+        <div className='model'></div>
+        <div className="shop"></div>
+        <div className="energy"></div>
+        <div className="suport"></div>
+      </section>
+
+      <section customer-center>
+        <div className='customer-text'></div>
+        <div className="company-number"></div>
+        <div className="company-hour"></div>
+        <div className="company-notice"></div>
+        <div className="company-tark"></div>
+        <div className="company-icon"></div>
+
+      </section>
+
+    </div>
   )
 }
 
